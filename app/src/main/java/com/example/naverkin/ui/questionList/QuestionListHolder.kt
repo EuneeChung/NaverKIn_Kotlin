639567b631d0d7cbcd70772cb8170beb44e23ec5
EuneeChung <chungee375@gmail.com>
2020-05-14 00:53:32 +0900

    view 수정

--- app/src/main/java/com/example/naverkin/ui/questionList/QuestionListHolder.kt
+++ app/src/main/java/com/example/naverkin/ui/questionList/QuestionListHolder.kt
@@ -1,8 +1,10 @@
 package com.example.naverkin.ui.questionList
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import android.view.View
 import android.widget.TextView
@@ -17,19 +19,18 @@ class QuestionListHolder(private  val view: View,private val context: Context):R
 
 
     val txtTitle: TextView=view.findViewById(R.id.txt_question_title)
-    val btnLink:TextView=view.findViewById(R.id.btn_question_link)
+
     val txtDesc:TextView=view.findViewById(R.id.txt_question_des)
 
+
     fun bind(data: RvQuestionListResponseItems){
-        txtTitle.text=data.title
-        txtDesc.text=data.description
-        btnLink.text=data.link
-        btnLink.setOnClickListener {
+        txtTitle.text=Html.fromHtml(data.title)
+        txtDesc.text=Html.fromHtml(data.description).slice(0..38)
+        txtTitle.setOnClickListener {
             val intent=Intent(context, WebActivity::class.java)
             intent.putExtra("link",data.link)
             startActivity(context,intent, Bundle())
             Log.e("link",data.link)
         }
     }
-
 }

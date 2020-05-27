5cf05a45547b31064ee72b80d9067743315fdc4f
EuneeChung <chungee375@gmail.com>
2020-05-28 05:36:57 +0900

    binding recycler view onclick

--- app/src/main/java/com/example/naverkin/data/QuestionListResponse.kt
+++ app/src/main/java/com/example/naverkin/data/QuestionListResponse.kt
@@ -3,6 +3,7 @@ package com.example.naverkin.data
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.core.content.ContextCompat.startActivity
 import com.example.naverkin.WebActivity
 
@@ -14,8 +15,16 @@ data class QuestionListResponse(
     val items:ArrayList<RvQuestionListResponseItems>
 )
 
-data class RvQuestionListResponseItems(
-    val title: String ="",
-    val link: String="" ,
+ class RvQuestionListResponseItems{
+    val title: String =""
+    val link: String=""
     val description: String=""
-)
+
+     fun onClickLink(view: View) {
+
+             val intent = Intent(view.context, WebActivity::class.java)
+             intent.putExtra("link", link)
+             startActivity(view.context, intent, Bundle())
+         }
+
+}
